19da1174e1a2dd0b10b5d1906c14fe94dcaef697
jmjubaer <jmjubaer3927@gmail.com>
2024-11-30 11:06:00 +0600

    create schema and model

---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+import { TProduct } from './products.interface';
+
+const productSchema = new Schema<TProduct>({
+  name: { type: String, required: true, trim: true },
+  brand: { type: String, required: true, trim: true },
+  price: { type: Number, required: true },
+  type: {
+    type: String,
+    enum: ['Mountain', 'Road', 'Hybrid', 'BMX', 'Electric'],
+    required: true,
+  },
+  description: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+export const Product = model<TProduct>('Product', productSchema);
