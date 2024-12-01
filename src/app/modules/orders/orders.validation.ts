e8fcf888c9de5d8220fd31d91172fd8eae16f119
jmjubaer <jmjubaer3927@gmail.com>
2024-12-01 09:38:10 +0600

    create the order validation schema

---
+++ src/app/modules/orders/orders.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const orderSchema = z.object({
+  email: z
+    .string()
+    .email("Invalid email format")
+    .nonempty("Email is required"),
+  product: z.string().nonempty("Product is required"),
+  quantity: z
+    .number()
+    .int("Quantity must be an integer")
+    .positive("Quantity must be a positive number")
+    .refine((val) => val > 0, { message: "Quantity is required" }),
+  totalPrice: z
+    .number()
+    .positive("TotalPrice must be a positive number")
+    .refine((val) => val > 0, { message: "TotalPrice is required" }),
+});
+
+export default orderSchema;
